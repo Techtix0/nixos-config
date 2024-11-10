9d2e10350ba072283f5e60acd58cbfe5f87e7ebf
Techtix0 <Techtix0@proton.me>
2024-11-10 18:00:43 +0100

    fixed spelling mistake

--- home.nix
+++ home.nix
@@ -1,27 +1,31 @@
-{ config, pkgs, userSettings, inputs, ... }:
-let
-
+{
+  config,
+  pkgs,
+  userSettings,
+  inputs,
+  ...
+}: let
 in {
   imports = [
-    ./style/stylix.nix  # Strylix settings
+    ./style/stylix.nix # Strylix settings
     # ./apps/terminal/bash.nix  # Bash config
-    ./apps/terminal/sh.nix  # Shell scripts
-    ./apps/terminal/kitty.nix  # terminal
+    ./apps/terminal/sh.nix # Shell scripts
+    ./apps/terminal/kitty.nix # terminal
     ./apps/utility/fuzzel.nix # application launcher
     ./apps/utility/ranger.nix # CLI file manager
-    ./desktop/wm/hyprland/hyprland.nix  # Window manager (also includes imports for hyprlock and hyprpaper)
-    ./desktop/waybar/waybar.nix  # waybar config
-    ./apps/utility/git.nix  # git/github config
-    ./apps/utility/neovim/neovim.nix  # neovim config
+    ./desktop/wm/hyprland/hyprland.nix # Window manager (also includes imports for hyprlock and hyprpaper)
+    ./desktop/waybar/waybar.nix # waybar config
+    ./apps/utility/git.nix # git/github config
+    ./apps/utility/neovim/neovim.nix # neovim config
   ];
 
   options = {};
 
-  config = {  
+  config = {
     # Home Manager needs a bit of information about you and the paths it should
     # manage.
     home.username = userSettings.username;
-    home.homeDirectory = "/home/"+userSettings.username;
+    home.homeDirectory = "/home/" + userSettings.username;
 
     # This value determines the Home Manager release that your configuration is
     # compatible with. This helps avoid breakage when a new Home Manager release
@@ -56,12 +60,12 @@ in {
     # Home Manager is pretty good at managing dotfiles. The primary way to manage
     # plain files is through 'home.file'.
     home.file = {
-		#	Neovim config files for init.lua
-			".config/nvim/lua" = {
-				source = ./apps/utility/neovim/lua;
-				recursive = true;
-			};
-		};
+      #	Neovim config files for init.lua
+      ".config/nvim/lua" = {
+        source = ./apps/utility/neovim/lua;
+        recursive = true;
+      };
+    };
 
     # Home Manager can also manage your environment variables through
     # 'home.sessionVariables'. These will be explicitly sourced when using a
@@ -83,12 +87,11 @@ in {
       # EDITOR = "nvim";  # Neovim is already set as default in ./apps/neovim/neovim.nix
     };
 
-    
     # Let Home Manager install and manage itself.
     programs.home-manager.enable = true;
 
     nixpkgs.config.allowUnfree = true;
-  
+
     programs.btop.enable = true;
-  };    
+  };
 }
