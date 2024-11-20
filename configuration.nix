822dbee94cfb5defb38157c48e7853b912c3c398
Techtix0 <Techtix0@proton.me>
2024-11-20 20:22:59 +0100

    enabled steam

--- configuration.nix
+++ configuration.nix
@@ -129,12 +129,12 @@
     ];
 
     #	Steam install
-    #programs.steam = {
-    #  enable = true;
-    #  remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
-    #  dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
-    #  localNetworkGameTransfers.openFirewall = true; # Open ports in the firewall for Steam Local Network Game Transfers
-    #};
+    programs.steam = {
+      enable = true;
+      remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
+      dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
+      localNetworkGameTransfers.openFirewall = true; # Open ports in the firewall for Steam Local Network Game Transfers
+    };
 
     # Removes unwanted KDE packages
     environment.plasma6.excludePackages = with pkgs.kdePackages; [
