eb5fbf55ea9c3559c8d1393f0f3de92a0fd10487
Techtix0 <Techtix0@proton.me>
2024-12-05 23:57:11 +0100

    added neovim keybinds for tab management

--- apps/utility/neovim/lua/vim-options.lua
+++ apps/utility/neovim/lua/vim-options.lua
@@ -3,9 +3,18 @@ vim.opt.tabstop = 2
 vim.opt.shiftwidth = 2
 vim.opt.softtabstop = 2
 vim.g.mapleader = " "
-vim.cmd.set "nowrap" 
+vim.cmd.set "nowrap"
 vim.cmd.set "nohlsearch"
 
 -- copy to clipboard
 vim.keymap.set('v', "<leader>y", '"+y')
+
+-- tabs
+vim.keymap.set('n', "<leader>tn", '<cmd>tabnew<cr>')
+vim.keymap.set('n', "<leader>tc", '<cmd>tabclose<cr>')
+vim.keymap.set('n', "<leader>tl", '<cmd>tabnext<cr>')
+vim.keymap.set('n', "<leader>th", '<cmd>tabprevious<cr>')
+vim.keymap.set('n', "<leader>to", '<cmd>tabonly<cr>')
+vim.keymap.set('n', "<leader>tml", '<cmd>+tabmove<cr>')
+vim.keymap.set('n', "<leader>tmh", '<cmd>-tabmove<cr>')
 
