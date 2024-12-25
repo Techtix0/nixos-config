57ce78bdbba46a3a7d024058bd905aad2d821701
Techtix0 <Techtix0@proton.me>
2024-12-25 03:29:49 +0100

    beginnings of starship config

--- apps/terminal/shell/starship.nix
+++ apps/terminal/shell/starship.nix
@@ -2,8 +2,25 @@
   programs.starship = {
     enable = true;
 
-		settings = {
-			add_newline = true;
-		};
+    settings = {
+      add_newline = true;
+      format = "$username | $directory$git_branch$git_commit \n$cmd_duration$character";
+
+      username = {
+				show_always = true;
+				style_user = "bold green";
+				format = "[$user]($style)";
+      };
+
+			directory = {
+				truncation_length = 1;	
+				truncation_symbol = "./";
+			};
+
+			cmd_duration = {
+				format = "[$duration]($style) ";
+			};
+
+    };
   };
 }
