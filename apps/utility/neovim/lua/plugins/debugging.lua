fdc72a8d4484cdb68d7af0fa87ce8cb900e201ee
Techtix0 <Techtix0@proton.me>
2025-01-21 23:34:42 +0100

    fixed stdin not working with debug

--- apps/utility/neovim/lua/plugins/debugging.lua
+++ apps/utility/neovim/lua/plugins/debugging.lua
@@ -23,6 +23,37 @@ return {
 			dapui.close()
 		end
 
+		dap.adapters.cppdbg = {
+		  id = 'cppdbg',
+		  type = 'executable',
+		  command = '/nix/store/gwags66qlqr6qmblwp0v6crkb6ca2qr1-vscode-extension-ms-vscode-cpptools-1.22.2/share/vscode/extensions/ms-vscode.cpptools/debugAdapters/bin/OpenDebugAD7',
+		}
+
+		dap.configurations.rust = {
+		  {
+		    name = "Launch file",
+		    type = "cppdbg",
+		    request = "launch",
+		    program = function()
+		      return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
+		    end,
+		    cwd = '${workspaceFolder}',
+		    stopAtEntry = true,
+		  },
+		  {
+		    name = 'Attach to gdbserver :1234',
+		    type = 'cppdbg',
+		    request = 'launch',
+		    MIMode = 'gdb',
+		    miDebuggerServerAddress = 'localhost:1234',
+		    miDebuggerPath = '/usr/bin/gdb',
+		    cwd = '${workspaceFolder}',
+		    program = function()
+		      return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
+		    end,
+		  },
+		}
+
 		vim.keymap.set('n', '<Leader>dt', dap.toggle_breakpoint, {})
 		vim.keymap.set('n', '<Leader>dc', dap.continue, {})
 	end
