f18b6b56f0e5ccbb2ecf07d9cd99c451f449d339
Techtix0 <techtix0@proton.me>
2025-02-24 22:25:12 +0100

    Build: secrets can be accessed in HM module

--- apps/utility/git.nix
+++ apps/utility/git.nix
@@ -1,8 +1,11 @@
-{config, ...}: 
-{
-	programs.git = {
-		enable = true;
-		userName = ''$(cat ${config.sops.secrets.git_username.path})'';
-		userEmail = ''$(cat ${config.sops.secrets.git_email.path})'';
-	};
+{config, ...}: {
+  config = {
+    programs.git = {
+      enable = true;
+      userName = ''${config.sops.secrets.git_username.path}'';
+      userEmail = ''${config.sops.secrets.git_email.path}'';
+    };
+    sops.secrets.git_username = {};
+    sops.secrets.git_email = {};
+  };
 }
