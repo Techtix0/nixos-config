7f4531225a94e8a6c246017be53263b5ab20eb69
Techtix0 <techtix0@proton.me>
2025-04-15 03:23:38 +0200

    Add: snippets

---
+++ apps/utility/neovim/lua/snippets/all.lua
@@ -0,0 +1,7 @@
+return {
+    -- To get the filtype use `:lua print(vim.inspect(require("luasnip").get_snippet_filetypes()))`
+    require("luasnip").snippet(
+        { trig = "helloworld", snippetType = "snippet", desc = "A hello world snippet", wordTrig = true },
+        { t("Just a hint of what's to come!"), }
+    ),
+}
