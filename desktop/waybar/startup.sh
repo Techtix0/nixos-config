2f8d722080473486cfbc29283d5645e8f629d989
Techtix0 <techtix0@proton.me>
2025-04-27 20:19:00 +0200

    Add: openrgb profile loading from startup

--- desktop/waybar/startup.sh
+++ desktop/waybar/startup.sh
@@ -4,4 +4,5 @@ killall .waybar-wrapped
 waybar &
 
 # sets rgb color of all devices to #463cbe at full brithness 
-openrgb -c 463cbe -m static -b 100 
+# openrgb -c 463cbe -m static -b 100 
+openrgb -p ../../nord.orp
