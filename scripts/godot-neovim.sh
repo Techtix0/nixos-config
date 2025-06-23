0bb2911ca9b1f46fc8d9af947403b3009911ee84
Techtix0 <techtix0@proton.me>
2025-06-23 02:02:51 +0200

    Add: bash script to use neovim with godot

---
+++ scripts/godot-neovim.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+# This script lets godot use neovim as an external editor
+# Change the following options in godot to use this script:
+#	editor settings -> general -> text editor -> external
+#	Exec path = /home/<username>/.dotfiles/scripts/godot-neovim.sh
+#	Exec flags = {file}
+#	Use external editor = true
+
+if [ ! -f /tmp/godot.pipe ]; then
+	# Start server if not running already
+	kitty nvim --listen /tmp/godot.pipe
+fi
+
+# Pass the required file to load in nvim	
+nvim --server /tmp/godot.pipe --remote-send "<C-\><C-N>:e $1<CR>" 
+
