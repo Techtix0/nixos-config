9b27f6b0468311eb6bfebeeb0554a6a11ddfd2a3
Techtix0 <techtix0@proton.me>
2025-07-01 21:23:47 +0200

    Update: moved tmux config to own file

--- apps/utility/tmux/tmux.nix
+++ apps/utility/tmux/tmux.nix
@@ -16,19 +16,7 @@
       plugins = with pkgs.tmuxPlugins; [
       ];
 
-      extraConfig = "
-					# Enables mouse support
-					set -g mouse on
-
-					# Sets vim keybinds to switch between panes
-					bind-key h select-pane -L
-					bind-key j select-pane -D
-					bind-key k select-pane -U
-					bind-key l select-pane -R
-
-					# Sets tmux status bar to top
-					set-option -g status-position top
-				";
+      extraConfig = lib.readFile ./tmux.conf;
     };
   };
 }
