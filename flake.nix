357937e37f03df2ac63e90c78b5a686daaa137a7
Techtix0 <techtix0@proton.me>
2025-07-08 20:59:59 +0200

    Update: reversed quickshell flake input

--- flake.nix
+++ flake.nix
@@ -25,7 +25,7 @@
 		};
 
 		quickshell = {
-			url = "github:quickshell-mirror/quickshell";
+			url = "git+https://git.outfoxxed.me/outfoxxed/quickshell";
 			inputs.nixpkgs.follows = "nixpkgs";
 		};
 
