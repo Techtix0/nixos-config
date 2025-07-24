96c5f1a41c564f8be15adfd0239e6ccac41b9ebd
Techtix0 <techtix0@proton.me>
2025-07-24 22:13:32 +0200

    WIP: got a nix-build to start

--- derivations/zlibrary.nix
+++ derivations/zlibrary.nix
@@ -1,23 +1,23 @@
-# stdenv, fetchurl, dpkg, autoPatchelfHook, qt5, ...
-{stdenv, fetchurl, dpkg, autoPatchelfHook, qt5, ...}:
-stdenv.mkDerivation rec {
+# stdenv, fetchurl, dpkg, autoPatchelfHook, qt5
+{pkgs ? import <nixpkgs> {}}:
+pkgs.stdenv.mkDerivation rec {
   pname = "Z-library";
   version = "2.4.3";
 
-  src = fetchurl {
+  src = pkgs.fetchurl {
     url = "https://s3proxy.cdn-zlib.sk/te_public_files/soft/linux/zlibrary-setup-latest.deb";
-    hash = "sha256-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA=";
+    hash = "sha256-OywGJdVUAGxK+C14akbLzhkt/5QE6+lchPHteksOLLY=";
   };
 
 	unpackCmd = "dpkg -x $curSrc source";
 
   nativeBuildInputs = [
-    dpkg
-		autoPatchelfHook
-		qt5.wrapQtAppsHook
+    pkgs.dpkg
+		pkgs.autoPatchelfHook
+		pkgs.qt5.wrapQtAppsHook
   ];
 
-  buildInputs = with qt5; [
+  buildInputs = with pkgs.qt5; [
     qtbase
 		qtsvg
 		qtwebengine
@@ -26,9 +26,11 @@ stdenv.mkDerivation rec {
 	installPhase = ''
 		runHook preInstall
 
-		mkdir -p $out/bin
-		mv usr/share opt $out
-		ln -s $out/opt/zlibrary-${version}/zlibrary $out/bin/zlibrary
+		mkdir -p $out
+		dpkg -x $src $out
+		cp -r usr/* $out
+
+		ln -s "$out/share/zlibrary/zlibrary" "$out/bin/zlibrary"
 
 		runHook postInstall
 	'';
