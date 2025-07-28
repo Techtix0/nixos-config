71953c2b8be932b4250fe9b26b1dce94a7c326bb
Techtix0 <techtix0@proton.me>
2025-07-28 07:51:49 +0200

    Add: changed corner radius of fuzzel menu to 0

--- apps/utility/fuzzel.nix
+++ apps/utility/fuzzel.nix
@@ -4,17 +4,10 @@
   home.packages = [pkgs.fuzzel];
   programs.fuzzel = {
     enable = true;
-    # settings = {
-    #   main.layer = "overlay";
-    #   colors = {
-    #     background = "111111ff";
-    #     text = "afafafff";
-    #     selection = "88C0D0ff";
-    #     match = "5E81ACff";
-    #     selection-match = "ffffffff";
-    #     selection-text = "000000ff";
-    #     border = "111111ff";
-    #   };
-    # };
+    settings = {
+			border = {
+				radius = 0;
+			};
+    };
   };
 }
