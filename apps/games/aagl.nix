8cbdf3c58b23263976f2092f28e0ef6f3d67987a
Techtix0 <techtix0@proton.me>
2025-09-06 21:30:45 +0200

    Add: sha256 to tarball

--- apps/games/aagl.nix
+++ apps/games/aagl.nix
@@ -4,7 +4,11 @@
   lib,
   ...
 }: let
-  aagl = import (builtins.fetchTarball "https://github.com/ezKEa/aagl-gtk-on-nix/archive/release-25.05.tar.gz");
+  aagl = import (builtins.fetchTarball {
+		url = "https://github.com/ezKEa/aagl-gtk-on-nix/archive/release-25.05.tar.gz";
+		sha256 = "sha256:1ibx1q0idrnin70dsp3brqzd432b2xd0vg3pz1l31v8afzlg7rp2";
+		}
+	);
 in {
   imports = [
 		aagl.module
