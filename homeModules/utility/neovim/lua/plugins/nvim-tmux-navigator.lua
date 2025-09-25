e5a5d0663ff2508518e606fabe6bb9e5e6123d9e
Techtix0 <techtix0@proton.me>
2025-09-26 01:34:25 +0200

    Add: nvim-tmux-navigator

---
+++ homeModules/utility/neovim/lua/plugins/nvim-tmux-navigator.lua
@@ -0,0 +1,9 @@
+-- Seemlessly switch between windows in tmux while using nvim
+return {
+	"christoomey/vim-tmux-navigator",
+	vim.keymap.set('n', '<C-h>', ':TmuxNavigateLeft<CR>'),
+	vim.keymap.set('n', '<C-l>', ':TmuxNavigateRight<CR>'),
+	vim.keymap.set('n', '<C-j>', ':TmuxNavigateDown<CR>'),
+	vim.keymap.set('n', '<C-k>', ':TmuxNavigateUp<CR>'),
+}
+
