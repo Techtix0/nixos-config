87de477da80bd69ace57de754231778526f5717c
Techtix0 <techtix0@proton.me>
2025-09-26 21:55:11 +0200

    Remove: luasnip snippets

--- homeModules/utility/neovim/lua/snippets/all.lua
+++
@@ -1,8 +0,0 @@
-return {
-    -- To get the filtype use `:lua print(vim.inspect(require("luasnip").get_snippet_filetypes()))`
-    require("luasnip").snippet(
-        { trig = "helloworld", snippetType = "snippet", desc = "A hello world snippet", wordTrig = true },
-        { t("Just a hint of what's to come!"), }
-    ),
-}
-
