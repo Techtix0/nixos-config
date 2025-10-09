2e2df025fc67f6f17c0b7d4c0a78646edc84c39e
Techtix0 <techtix0@proton.me>
2025-10-09 23:04:28 +0200

    Fix: formatted aagl.nix and removed unecessary program

--- modules/games/aagl.nix
+++ modules/games/aagl.nix
@@ -4,21 +4,21 @@
   lib,
   ...
 }: let
-  aagl = import (builtins.fetchTarball {
-		url = "https://github.com/ezKEa/aagl-gtk-on-nix/archive/release-25.05.tar.gz";
-		sha256 = "sha256:1ibx1q0idrnin70dsp3brqzd432b2xd0vg3pz1l31v8afzlg7rp2";
-		}
-	);
+  aagl = import (
+    builtins.fetchTarball {
+      url = "https://github.com/ezKEa/aagl-gtk-on-nix/archive/release-25.05.tar.gz";
+      sha256 = "sha256:1ibx1q0idrnin70dsp3brqzd432b2xd0vg3pz1l31v8afzlg7rp2";
+    }
+  );
 in {
   imports = [
-		aagl.module
-	];
+    aagl.module
+  ];
 
   options = {};
 
   config = {
-		nix.settings = aagl.nixConfig; # Set up Cachix
-	  programs.anime-game-launcher.enable = true;
-	  programs.anime-games-launcher.enable = true;
-	};
+    nix.settings = aagl.nixConfig; # Set up Cachix
+    programs.anime-games-launcher.enable = true;
+  };
 }
