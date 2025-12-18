2dfed2cc30bef1885b01f46136fe5d51fe580af5
Techtix0 <techtix0@proton.me>
2025-12-18 18:38:09 +0100

    Fix: updated git attributes

--- homeModules/utility/git.nix
+++ homeModules/utility/git.nix
@@ -2,9 +2,13 @@
   config = {
     programs.git = {
       enable = true;
-      userName = ''${config.sops.secrets.git_username.path}'';
-      userEmail = ''${config.sops.secrets.git_email.path}'';
-    };
+			settings = {
+				user = {
+					email = ''${config.sops.secrets.git_email.path}'';
+      		name = ''${config.sops.secrets.git_username.path}'';
+				};
+			};    
+		};
     sops.secrets.git_username = {};
     sops.secrets.git_email = {};
   };
