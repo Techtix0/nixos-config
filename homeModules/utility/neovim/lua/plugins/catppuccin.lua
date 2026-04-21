0df57c75ad5686e68589f4949aac415f2270a6a0
Techtix0 <techtix0@proton.me>
2026-04-21 20:13:03 +0200

    Add: clearer line numbering

--- homeModules/utility/neovim/lua/plugins/catppuccin.lua
+++ homeModules/utility/neovim/lua/plugins/catppuccin.lua
@@ -2,10 +2,19 @@ return {
 	"catppuccin/nvim",
 	lazy = false,
 	name = "catppuccin",
-	priority = 1000, 
+	priority = 1000,
 	config = function()
 		-- require("catppuccin").setup({flavour = "mocha"})
-		require("catppuccin").setup({flavour = "frappe"})
+		require("catppuccin").setup({
+			flavour = "frappe",
+			highlight_overrides = {
+				frappe = function(frappe)
+            return {
+                LineNr = { fg = frappe.subtext0},
+            }
+        end,
+			}
+		})
 		vim.cmd.colorscheme "catppuccin"
 	end
 }
