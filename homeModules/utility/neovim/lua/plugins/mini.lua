c7b5575354c838e0dfb002e5005024bdc7b24e85
Techtix0 <techtix0@proton.me>
2026-04-21 21:06:35 +0200

    Fix: syntax from previous commit

--- homeModules/utility/neovim/lua/plugins/mini.lua
+++ homeModules/utility/neovim/lua/plugins/mini.lua
@@ -18,19 +18,21 @@ return {
 			require("mini.pairs").setup()
 		end
 	},
-	-- Align plugin to simply align text.
-	-- Start aligning selected text with "ga".
-		-- Press "s" to enter split Lua pattern.
-		-- Press "j" to choose justification side from available ones (“left”, “center”, “right”, “none”).
-		-- Press "m" to enter merge delimiter.
-		-- Press "f" to enter filter Lua expression to configure which parts will be affected (like “align only first column”).
-		-- Press "i" to ignore some commonly unwanted split matches.
-		-- Press "p" to pair neighboring parts so they be aligned together.
-		-- Press "t" to trim whitespace from parts.
-		-- Press "<BS>" (backspace) to delete some last pre-step.
+	{
+		-- Align plugin to simply align text.
+		-- Start aligning selected text with "ga".
+			-- Press "s" to enter split Lua pattern.
+			-- Press "j" to choose justification side from available ones (“left”, “center”, “right”, “none”).
+			-- Press "m" to enter merge delimiter.
+			-- Press "f" to enter filter Lua expression to configure which parts will be affected (like “align only first column”).
+			-- Press "i" to ignore some commonly unwanted split matches.
+			-- Press "p" to pair neighboring parts so they be aligned together.
+			-- Press "t" to trim whitespace from parts.
+			-- Press "<BS>" (backspace) to delete some last pre-step.
 		"nvim-mini/mini.align", version = "*",
 		config = function ()
 			require("mini.align").setup()
 		end
 	}
 }
+
