0013c35a8542c291b71ddc6386dbf6832917c594
Techtix0 <techtix0@proton.me>
2026-04-21 20:55:13 +0200

    Add: mini.pair plugin for neovim

--- homeModules/utility/neovim/lua/plugins/mini.lua
+++ homeModules/utility/neovim/lua/plugins/mini.lua
@@ -1,12 +1,23 @@
 return {
-	-- Surround plugin for surround actions
-		-- Add surrounding with `sa` (in visual mode or on motion).
-		-- Delete surrounding with `sd`.
-		-- Replace surrounding with `sr`.
-		-- Find surrounding with sf or `sF` (move cursor right or left).
-		-- Highlight surrounding with `sh`.
-	"nvim-mini/mini.surround", version = false,
-	config = function()
-		require("mini.surround").setup()
-	end
+	{
+		-- Surround plugin for surround actions
+			-- Add surrounding with `sa` (in visual mode or on motion).
+			-- Delete surrounding with `sd`.
+			-- Replace surrounding with `sr`.
+			-- Find surrounding with sf or `sF` (move cursor right or left).
+			-- Highlight surrounding with `sh`.
+		"nvim-mini/mini.surround", version = "*",
+		config = function()
+			require("mini.surround").setup()
+		end,
+	},
+	{
+		-- Pairs plugin for auto-pairing
+		"nvim-mini/mini.pairs", version = "*",
+		config = function ()
+			require("mini.pairs").setup()
+		end
+	}
+
+
 }
