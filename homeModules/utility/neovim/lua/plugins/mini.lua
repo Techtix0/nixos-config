8498e2ddfedcdcaaa5b6127aa369997cbcd5d535
Techtix0 <techtix0@proton.me>
2026-04-21 21:04:46 +0200

    Add: mini.align plugin for neovim

--- homeModules/utility/neovim/lua/plugins/mini.lua
+++ homeModules/utility/neovim/lua/plugins/mini.lua
@@ -1,6 +1,6 @@
 return {
 	{
-		-- Surround plugin for surround actions
+		-- Surround plugin for surround actions.
 			-- Add surrounding with `sa` (in visual mode or on motion).
 			-- Delete surrounding with `sd`.
 			-- Replace surrounding with `sr`.
@@ -12,12 +12,25 @@ return {
 		end,
 	},
 	{
-		-- Pairs plugin for auto-pairing
+		-- Pairs plugin for auto-pairing.
 		"nvim-mini/mini.pairs", version = "*",
 		config = function ()
 			require("mini.pairs").setup()
 		end
+	},
+	-- Align plugin to simply align text.
+	-- Start aligning selected text with "ga".
+		-- Press "s" to enter split Lua pattern.
+		-- Press "j" to choose justification side from available ones (“left”, “center”, “right”, “none”).
+		-- Press "m" to enter merge delimiter.
+		-- Press "f" to enter filter Lua expression to configure which parts will be affected (like “align only first column”).
+		-- Press "i" to ignore some commonly unwanted split matches.
+		-- Press "p" to pair neighboring parts so they be aligned together.
+		-- Press "t" to trim whitespace from parts.
+		-- Press "<BS>" (backspace) to delete some last pre-step.
+		"nvim-mini/mini.align", version = "*",
+		config = function ()
+			require("mini.align").setup()
+		end
 	}
-
-
 }
