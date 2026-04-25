3d767492c5d89f282daf6107357d8b3c4c6cc72a
Techtix0 <techtix0@proton.me>
2026-04-25 19:02:24 +0200

    Remove: debugging in neovim

--- homeModules/utility/neovim/lua/plugins/mini.lua
+++ homeModules/utility/neovim/lua/plugins/mini.lua
@@ -4,7 +4,7 @@ return {
 			-- Add surrounding with `sa` (in visual mode or on motion).
 			-- Delete surrounding with `sd`.
 			-- Replace surrounding with `sr`.
-			-- Find surrounding with sf or `sF` (move cursor right or left).
+			-- Find surrounding with `sf` or `sF` (move cursor right or left).
 			-- Highlight surrounding with `sh`.
 		"nvim-mini/mini.surround", version = "*",
 		config = function()
